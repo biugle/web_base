c19d543dee4db908edbc47edea0f591a0a3e4330
doubleam <admin@biugle.cn>
2023-06-07 15:33:11 +0800

    Feat: 增加 electron 支持 service worker 与  SSL 证书生成

--- electron/main.ts
+++ electron/main.ts
@@ -2,11 +2,14 @@
  * @Author: HxB
  * @Date: 2022-08-15 15:42:27
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-05-31 14:33:57
+ * @LastEditTime: 2023-06-07 15:28:49
  * @Description: electron 打包与启动文件主程序
  * @FilePath: \web_base\electron\main.ts
  */
 import * as path from 'path';
+import * as https from 'https';
+import * as http from 'http';
+import * as fs from 'fs';
 import { app, BrowserWindow, globalShortcut } from 'electron';
 import { initEvents } from './events';
 
@@ -16,6 +19,52 @@ const mode = process.argv[2]; // process.env.NODE_ENV
 // const baseDir = path.dirname(process.execPath);
 // const baseRenderDir = path.join(baseDir, 'resources/app');
 // const bgPng = path.join(baseRenderDir, 'source', 'bg.png');
+
+// eslint-disable-next-line no-undef
+const getDistDir = (filePath: string) => path.join(__dirname, '../', filePath);
+
+const initServer = () => {
+  const sslOptions: any = {};
+  try {
+    sslOptions.key = fs.readFileSync(getDistDir('.ssl/server.key'));
+    sslOptions.cert = fs.readFileSync(getDistDir('.ssl/server.cert'));
+  } catch (err) {
+    console.log('SSL Options error!', err);
+  }
+
+  return https.createServer(sslOptions, async (req: any, res) => {
+    // return http.createServer(async (req: any, res) => {
+    req.url = req.url == '/' ? '/index.html' : req.url;
+
+    // eslint-disable-next-line no-undef
+    const filePath = getDistDir(req.url);
+
+    const ext = path.extname(filePath);
+
+    let contentType;
+    switch (ext) {
+      case '.js':
+        contentType = 'application/javascript';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.html':
+        contentType = 'text/html';
+    }
+    if (contentType) {
+      res.setHeader('Content-Type', `${contentType};charset=utf-8;`);
+    }
+
+    try {
+      const file = await fs.readFileSync(filePath);
+      res.end(file);
+    } catch (e) {
+      console.log({ e }, '读取资源失败或找不到资源');
+      // 显示错误到页面
+    }
+  });
+};
 
 let mainWindow: BrowserWindow;
 
@@ -38,13 +87,22 @@ const createWindow = () => {
       // contextIsolation: false, // preload 单独的运行环境
     },
   });
+
   console.log(mode);
+
   if (!app.isPackaged && !mode.includes('prod')) {
     // eslint-disable-next-line no-undef
     mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL ?? 'http://localhost:1998/');
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile('dist/index.html').catch(() => null);
+    const customServer = initServer();
+    customServer.listen(9443);
+    mainWindow.loadURL('https://localhost:9443/');
+
+    // customServer.listen(9080);
+    // mainWindow.loadURL('http://localhost:9080/');
+
+    // mainWindow.loadFile('dist/index.html').catch(() => null);
   }
 
   // 关闭 window 时触发下列事件
@@ -58,6 +116,8 @@ const createWindow = () => {
 };
 
 app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors'); // 兼容各个版本关闭跨域(上线时删除此配置)
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true'); // 忽略证书校验
+app.commandLine.appendSwitch('disable-extensions'); // 禁用 Chrome 扩展
 
 // 绑定 ready 方法，当 electron 应用创建成功时，创建一个窗口。
 app.whenReady().then(() => {
