26ebb6d4db985c5dc67952caa21cf1429a46178c
doubleam <admin@biugle.cn>
2023-06-14 18:08:29 +0800

    Feat: 增加 vConsole 支持

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,13 +2,13 @@
  * @Author: HxB
  * @Date: 2023-04-27 15:38:29
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-06-14 17:25:31
+ * @LastEditTime: 2023-06-14 18:08:00
  * @Description: 首页
  * @FilePath: \web_base\src\views\Home\index.tsx
  */
 import React, { useEffect } from 'react';
 import { connect, useDispatch, useSelector } from 'react-redux';
-import { Button, Result } from 'antd';
+import { Button, Result, Space } from 'antd';
 import { useAliveController } from 'react-activation';
 import { actions, selectors } from '@store/all';
 import AntIcon from '@components/AntIcon';
@@ -35,14 +35,42 @@ const Home = (props: any) => {
         title="403"
         subTitle="Sorry, you are not authorized to access this page.(TEST)"
         extra={
-          <Button
-            type="primary"
-            onClick={() => {
-              props.history.push('/login');
-            }}
-          >
-            Back Login
-          </Button>
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => {
+                props.history.push('/login');
+              }}
+            >
+              Back Login
+            </Button>
+            <Button
+              ghost
+              type="primary"
+              onClick={() => {
+                // @ts-ignore
+                if (window.myVConsole) {
+                  // @ts-ignore
+                  window.myVConsole.show();
+                }
+              }}
+            >
+              vConsole Open
+            </Button>
+            <Button
+              danger
+              type="ghost"
+              onClick={() => {
+                // @ts-ignore
+                if (window.myVConsole) {
+                  // @ts-ignore
+                  window.myVConsole.destroy();
+                }
+              }}
+            >
+              vConsole Destroy
+            </Button>
+          </Space>
         }
       />
     </div>
