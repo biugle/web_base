269f9e6c6edc16129ae55fa17879084af9b86e9c
doubleam <admin@biugle.cn>
2023-06-16 14:57:28 +0800

    Docs: 统一用词
    (cherry picked from commit 655537e2243059b06739035ad03bba75dca6fdc6)

--- _template_/app-hook.ts
+++ _template_/app-hook.ts
@@ -2,8 +2,8 @@
  * @Author: HxB
  * @Date: 2023-04-27 15:38:29
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-05-25 18:04:24
- * @Description: hook 模块
+ * @LastEditTime: 2023-06-16 14:53:23
+ * @Description: hook 模板
  * @FilePath: \web_base\_template_\app-hook.ts
  */
 const useXxx = () => {
