9d5e3a30529ac11f8aa72c5ca184e97f60bbef89
doubleam <admin@biugle.cn>
2023-07-31 15:39:41 +0800

    Feat: 登录页面调整优化，增加记住账号、忘记密码等。
    (cherry picked from commit 52c28f6a5ac9c5be64e3d0025371927f38e32932)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,11 +2,11 @@
  * @Author: HxB
  * @Date: 2023-04-27 15:32:21
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-05-23 14:14:00
+ * @LastEditTime: 2023-07-31 15:16:58
  * @Description: 登录页面
  * @FilePath: \web_base\src\pages\Login\index.tsx
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './style.less';
 import { useAliveController } from 'react-activation';
@@ -17,6 +17,7 @@ import logo from '@resource/imgs/logo-white.png';
 const Login = () => {
   const HAS_REGISTER = true;
   const history = useHistory();
+  const [isSaveAccount, setIsSaveAccount] = useState<boolean>(window.localStorage.getItem('isSaveAccount') === 'true');
   const onBlur = (e: any) => {
     !e.target.value && e.target.classList.remove('focus');
   };
@@ -47,19 +48,19 @@ const Login = () => {
           <form>
             <h1>注册</h1>
             <div className="txtb">
-              <input type="text" onFocus={onFocus} onBlur={onBlur} />
-              <span data-placeholder="Username"></span>
+              <input autoComplete="off" type="text" onFocus={onFocus} onBlur={onBlur} />
+              <span data-placeholder="Account"></span>
             </div>
             <div className="txtb">
-              <input type="email" onFocus={onFocus} onBlur={onBlur} />
+              <input autoComplete="off" type="email" onFocus={onFocus} onBlur={onBlur} />
               <span data-placeholder="Email"></span>
             </div>
             <div className="txtb">
-              <input type="password" onFocus={onFocus} onBlur={onBlur} />
+              <input autoComplete="off" type="password" onFocus={onFocus} onBlur={onBlur} />
               <span data-placeholder="Password"></span>
             </div>
             <div className="txtb">
-              <input type="password" onFocus={onFocus} onBlur={onBlur} />
+              <input autoComplete="off" type="password" onFocus={onFocus} onBlur={onBlur} />
               <span data-placeholder="Confirm Password"></span>
             </div>
             <button
@@ -76,22 +77,35 @@ const Login = () => {
           <form action="#">
             <h1>登录</h1>
             <div className="txtb">
-              <input type="email" onFocus={onFocus} onBlur={onBlur} />
-              <span data-placeholder="Email"></span>
+              <input autoComplete="off" type="email" onFocus={onFocus} onBlur={onBlur} />
+              <span data-placeholder="Email/Account"></span>
             </div>
             <div className="txtb">
-              <input type="password" onFocus={onFocus} onBlur={onBlur} />
+              <input autoComplete="off" type="password" onFocus={onFocus} onBlur={onBlur} />
               <span data-placeholder="Password"></span>
             </div>
-            <a
-              href="#"
-              onClick={(e: any) => {
-                stopEvent(e);
-                history.push('/404');
-              }}
-            >
-              忘记密码？
-            </a>
+            <div className="toolbox">
+              <label className="save-account-btn">
+                <input
+                  type="checkbox"
+                  checked={isSaveAccount}
+                  onChange={(e: any) => {
+                    setIsSaveAccount(e.target.checked);
+                    window.localStorage.setItem('isSaveAccount', e.target.checked);
+                  }}
+                />
+                <span>记住账号</span>
+              </label>
+              <a
+                href="#"
+                onClick={(e: any) => {
+                  stopEvent(e);
+                  history.push('/404');
+                }}
+              >
+                忘记密码？
+              </a>
+            </div>
             <button
               onClick={(e: any) => {
                 stopEvent(e);
@@ -106,8 +120,8 @@ const Login = () => {
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>已有帐号？</h1>
-              <p>请使用您的帐号进行登录</p>
+              <h1>已有账号？</h1>
+              <p>请使用您的账号进行登录</p>
               <button
                 className="ghost"
                 id="signIn"
@@ -121,7 +135,7 @@ const Login = () => {
             </div>
             {HAS_REGISTER ? (
               <div className="overlay-panel overlay-right">
-                <h1>没有帐号?</h1>
+                <h1>没有账号?</h1>
                 <p>立即注册，和我们一起开始旅程吧！</p>
                 <button
                   className="ghost"
