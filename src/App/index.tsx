0c43fc3411f43fcf5f05d150ce74cfe085f9cfad
doubleam <admin@biugle.cn>
2023-08-18 15:39:55 +0800

    Feat: 路由渲染与校验优化
    (cherry picked from commit 1ae419848f908e4a74300569c804430af075a665)
    (cherry picked from commit 29d7de1ff37669dc40d28373cbefcc6398f3d667)

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,7 +2,7 @@
  * @Author: HxB
  * @Date: 2023-04-27 15:38:29
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-05-31 14:48:06
+ * @LastEditTime: 2023-08-18 15:24:46
  * @Description: 主组件
  * @FilePath: \web_base\src\App\index.tsx
  */
@@ -11,6 +11,8 @@ import AppLoading from '@components/AppLoading';
 import './style.less';
 import AppRouter from '@router/AppRouter';
 import { log } from 'js-xxx';
+import { connect } from 'react-redux';
+import { selectors } from '@/store/all';
 
 class App extends Component<any, { loading: boolean }> {
   constructor(props: any) {
@@ -32,12 +34,44 @@ class App extends Component<any, { loading: boolean }> {
   }
 
   render() {
+    function getRoutes(routes, userRoles) {
+      userRoles = userRoles ?? [];
+      const res = [];
+      routes.forEach((r) => {
+        if (r.routes) {
+          const _data = getRoutes(r.routes ?? [], userRoles);
+          if (_data.length > 0) {
+            res.push({
+              ...r,
+              routes: _data,
+            });
+          }
+          return;
+        }
+        if (!r.roles) {
+          res.push(r);
+          return;
+        }
+        if ([].concat(r.roles)?.some((role) => userRoles.includes(role))) {
+          res.push(r);
+          return;
+        }
+      });
+      return res;
+    }
+
+    const { userRoles } = this.props;
+
     if (this.state.loading) {
       return <AppRouter routes={this.props.routes} />;
+      // return <AppRouter routes={getRoutes(this.props.routes, userRoles)} />;
     }
 
     return <AppLoading />;
   }
 }
 
-export default App;
+export default connect((state, ownProps: any) => ({
+  // ...selectors.user(state),
+  routes: ownProps.routes, // 使用从属性传递过来的路由配置
+}))(App);
