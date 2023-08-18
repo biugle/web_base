a041eb4e8b26c756500fd96d4fb5b7a965f02918
doubleam <admin@biugle.cn>
2023-08-18 15:39:55 +0800

    Feat: 路由渲染与校验优化

--- src/router/AppRouter/AuthRouteDom.tsx
+++ src/router/AppRouter/AuthRouteDom.tsx
@@ -2,7 +2,7 @@
  * @Author: HxB
  * @Date: 2023-04-27 15:16:18
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-05-08 16:23:38
+ * @LastEditTime: 2023-08-18 15:19:33
  * @Description: 路由守卫子组件
  * @FilePath: \web_base\src\router\AppRouter\AuthRouteDom.tsx
  */
@@ -10,7 +10,8 @@ import React, { Component } from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
 import KeepAlive from 'react-activation';
 import { DEV_ROLES, IS_DEV } from '@_custom/config';
-import store, { actions } from '@store/all';
+import store, { actions, selectors } from '@store/all';
+import { connect } from 'react-redux';
 
 class AuthRoute extends Component<any, any> {
   constructor(props: any) {
@@ -18,15 +19,15 @@ class AuthRoute extends Component<any, any> {
   }
 
   render() {
-    const { path, roles } = this.props;
+    const { path, roles, userRoles } = this.props;
 
     // alert(JSON.stringify(this.props));
 
     // 用户角色，全局状态管理可自行引入 redux 。
-    const userRoles = IS_DEV ? (!DEV_ROLES || !DEV_ROLES.length ? undefined : DEV_ROLES) : undefined;
+    const authRoles = !userRoles && IS_DEV ? (!DEV_ROLES || !DEV_ROLES.length ? undefined : DEV_ROLES) : userRoles; // userRoles 自行引入或定义
 
     // 用户已登录，还想去登录页面。禁止
-    if (userRoles && path === '/login') {
+    if (authRoles && path === '/login') {
       return <Redirect to="/" />;
     }
 
@@ -52,17 +53,17 @@ class AuthRoute extends Component<any, any> {
     }
 
     // 用户未登录，且路由需要校验，则跳转到登录页。
-    if (!userRoles) {
+    if (!authRoles) {
       return <Redirect to="/login" />;
     }
 
     // 路由需要校验，且用户已登录。有权限进入，无权限 404 。
-    if (roles?.some((role: any) => userRoles?.includes(role))) {
+    if ([].concat(roles)?.some((role: any) => authRoles?.includes(role))) {
       return $AuthRouteDom;
     } else {
       return <Redirect to="/404" />;
     }
   }
 }
-
+// export default connect(selectors.user)(withRouter(AuthRoute));
 export default withRouter(AuthRoute);
