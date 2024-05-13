63466b7d35b58a3791a12985971979fc605a1f15
hank.he <hank.he@imile.me>
2024-05-13 11:07:57 +0800

    Feat: 调整整体配置与代码

--- src/_custom/hooks/useLatest.ts
+++ src/_custom/hooks/useLatest.ts
@@ -2,11 +2,11 @@
  * @Author: HxB
  * @Date: 2023-04-27 14:42:28
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-05-08 16:26:44
+ * @LastEditTime: 2024-05-09 14:19:41
  * @Description: useLatest 自定义 hooks
  * @FilePath: \web_base\src\_custom\hooks\useLatest.ts
  */
-import { useRef } from 'react';
+import { useRef, useLayoutEffect } from 'react';
 
 const useLatest = <T>(value: T) => {
   const ref = useRef(value);
@@ -14,5 +14,15 @@ const useLatest = <T>(value: T) => {
 
   return ref;
 };
+
+// export function useLatest<T>(value: T) {
+//   const ref = useRef(value);
+
+//   useLayoutEffect(() => {
+//     ref.current = value;
+//   });
+
+//   return ref;
+// }
 
 export default useLatest;
