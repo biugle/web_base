9eaa1ca1f1883ce1e35cbdbd56e8efa7c209e5f2
bex <admin@biugle.cn>
2025-03-06 16:55:54 +0800

    Fix: box bug with useMemo
    (cherry picked from commit b27bb5fb4e5745b94e288cc2422fc9fe4f484f0e)

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -3,14 +3,24 @@ import styled from '@emotion/styled';
 import { cx } from '@emotion/css';
 
 interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
-  sx?: Record<string, any>;
+  sx?: any;
   className?: string;
+  children?: React.ReactNode;
+  [key: string]: any;
 }
 
-const Box: React.FC<BoxProps> = ({ sx, className, ...props }) => {
-  const BoxElement = styled('div')(sx);
+// 将 styled 组件移到组件外部
+const createStyledElement = (styles: any) => styled.div(styles);
 
-  return <BoxElement className={cx(className, 'BoxElement')} {...props} />;
+const Box: React.FC<BoxProps> = ({ sx, className, children, ...props }) => {
+  // 使用 useMemo 缓存 styled 组件，避免每次渲染重新创建 styled 组件，影响 dom tree 的构建。
+  const BoxElement = React.useMemo(() => createStyledElement(sx), [sx]);
+
+  return (
+    <BoxElement className={cx(className, 'BoxElement')} {...props}>
+      {children}
+    </BoxElement>
+  );
 };
 
 export default Box;
